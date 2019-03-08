08a1d635bce588981aa1c1271f64f4ceb36a4afd
Yatan Vesh <yatan.vesh@gmail.com>
2019-03-08 22:26:25 +0530

    Added registeruser route

--- server.js
+++ server.js
@@ -8,21 +8,82 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+let succObj = {
+    success:true
+}
+
+const db = knex({
+    client: 'pg',
+    connection:{
+        host:'127.0.0.1',
+        user: "gerontius",
+        password:'',
+        database:'silicon'
+    }
+});
+
 app.get('/', (req,res) =>{
+
     res.send("success");
 });
 
-app.post('/register', (req,res) =>{
-    const {email,name,password} = req.body;
-    console.log(email,name,password);
-    let obj ={
-        email:email,
-        name:name,
-        password:password
+let obj = null;
+
+app.post('/registeruser', (req,res) =>{
+    const {
+        username,
+        email,
+        password,
+        age,
+        sex,
+        phone,
+        medcondition,
+        ilnesses,
+        address,
+        phoneemergency,
+        bloodgroup,
+        medicineintolerance,
+        medication,
+        special
+    } = req.body;
+    console.log('registeruser post name:', username);
+    obj ={
+        username,
+        email,
+        password,
+        age,
+        sex,
+        phone,
+        medcondition,
+        ilnesses,
+        address,
+        phoneemergency,
+        bloodgroup,
+        medicineintolerance,
+        medication,
+        special
     }
-    res.json(obj);
+    db('users').insert({
+        username:username,
+        email:email,
+        passwordhash:password,
+        age:age,
+        sex:sex,
+        phone:phone,
+        medcondition:medcondition,
+        ilnesses:ilnesses,
+        address:address,
+        phoneemergency:phoneemergency,
+        bloodgroup:bloodgroup,
+        medicineintolerance:medicineintolerance,
+        medication:medication,
+        special:special
+    })
+    .catch( err => console.log(err));
+    
+    res.json(succObj);
 })
 
-app.listen(3005, ()=>{ 
-    console.log("app running on 3005");
+app.listen(3006, ()=>{ 
+    console.log("REST API Server Running on PORT 3006");
 })
